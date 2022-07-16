154e5fab4fa08138d8fe89db06aa170e03b54c5f
happydtd <happydtdau@yahoo.com.au>
2022-07-16 17:40:53 +1000

    section 5 finish

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,19 +1,26 @@
+using Discount.API.Repositories;
+using Discount.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Discount.API", Version = "v1" });
+});
 
 var app = builder.Build();
 
+app.MigrateDatabase<Program>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Discount.API v1"));
 }
 
 app.UseAuthorization();
