1416907408990f5d0fc14ead691695eb76bb41a7
happydtd <happydtdau@yahoo.com.au>
2022-07-19 08:54:39 +1000

    98 mid

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Behaviours
+{
+    public class ValidationBehaviour<Trequest, Tresponse> : IPipelineBehavior<Trequest, Tresponse>
+    {
+        private readonly IEnumerable<IValidator<Trequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<Trequest>> validators)
+        {
+            _validators = validators?? throw new ArgumentNullException(nameof(validators));
+        }
+
+        public async Task<Tresponse> Handle(Trequest request, CancellationToken cancellationToken, RequestHandlerDelegate<Tresponse> next)
+        {
+            if (_validators.Any())
+            {
+
+            }
+        }
+    }
+}
