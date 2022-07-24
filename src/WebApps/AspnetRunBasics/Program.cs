a425f8c92761aa552a66bf1e5c3903377be9be53
happydtd <happydtdau@yahoo.com.au>
2022-07-24 18:07:16 +1000

    finish

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,39 +1,16 @@
-using AspnetRunBasics.Data;
-using AspnetRunBasics.Repositories;
-using Microsoft.EntityFrameworkCore;
+using AspnetRunBasics.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 var Configuration = builder.Configuration;
 
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(c => c.BaseAddress = new Uri(Configuration["ApiSettings:GatewayAddress"]));
+builder.Services.AddHttpClient<IBasketService, BasketService>(c => c.BaseAddress = new Uri(Configuration["ApiSettings:GatewayAddress"]));
+builder.Services.AddHttpClient<IOrderService, OrderService>(c => c.BaseAddress = new Uri(Configuration["ApiSettings:GatewayAddress"]));
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<AspnetRunContext>(c => 
-    c.UseSqlServer(Configuration.GetConnectionString("AspnetRunConnection")));
 
-// add repository dependecy
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<ICartRepository, CartRepository>();
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-builder.Services.AddScoped<IContactRepository, ContactRepository>();
 
 var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-
-    try
-    {
-        var aspnetRunContext = services.GetRequiredService<AspnetRunContext>();
-        AspnetRunContextSeed.SeedAsync(aspnetRunContext, loggerFactory).Wait();
-    }
-    catch (Exception exception)
-    {
-        var logger = loggerFactory.CreateLogger<Program>();
-        logger.LogError(exception, "An error occurred seeding the DB.");
-    }
-}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
